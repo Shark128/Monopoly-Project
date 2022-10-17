0d6e4f7664b26e7e94a7a59a20c785d72ba6a373
Shark128 <69443118+Shark128@users.noreply.github.com>
2022-10-17 15:24:03 -0500

    Comment

--- Player.java
+++ Player.java
@@ -1,3 +1,13 @@
+import java.util.*;
 public class Player {
+    public int balance;
+    public ArrayList<BoardSpace> properties;
+    public char icon;
+
+    public Player (char icon){
+        balance = 1500;
+        properties = new ArrayList<BoardSpace>();
+        this.icon =  icon;
+    }
 
 }
