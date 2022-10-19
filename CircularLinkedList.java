f10fc8e39af6839f0d0e296533c1276e2b148e1a
Shark128 <69443118+Shark128@users.noreply.github.com>
2022-10-19 12:13:33 -0500

    CircLink

--- CircularLinkedList.java
+++ CircularLinkedList.java
@@ -2,5 +2,20 @@ public class CircularLinkedList {
     public Link firstLink;
     public Link lastLink;
     public CircularLinkedList () {firstLink = null; lastLink = null;}
+    public void insertFirst (Link newLink) {
+        if (isEmpty()) {
+            firstLink = newLink;
+            lastLink = newLink;
+        }
+        else {
+            Link oldFirstLink = firstLink;
+            firstLink = newLink;
+            firstLink.nextLink = oldFirstLink;
+            lastLink.nextLink = firstLink;
+        }
+    }
+    public boolean isEmpty () {
+        return firstLink == null;
+    }
 
 }
