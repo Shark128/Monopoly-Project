d08d390f518af47dc4bdca80883b76dc2b14135b
Shark128 <69443118+Shark128@users.noreply.github.com>
2022-10-19 12:08:19 -0500

    Link

--- CircularLinkedList.java
+++ CircularLinkedList.java
@@ -1,3 +1,6 @@
 public class CircularLinkedList {
+    public Link firstLink;
+    public Link lastLink;
+    public CircularLinkedList () {firstLink = null; lastLink = null;}
 
 }
--- Link.java
+++ Link.java
@@ -4,4 +4,5 @@ public class Link<D> {
     public Link (D data) {
         this.data = data;
     }
+
 }
