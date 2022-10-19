e448eb094fb0d5e3c9c3139b64c207ac593ad289
Bobthebri <111515866+Bobthebri@users.noreply.github.com>
2022-10-19 12:46:38 -0500

    Board Spaces

--- Main.java
+++ Main.java
@@ -19,5 +19,22 @@ public class Main {
                 6, 1);
         BoardSpace Go_To_Jail = new BoardSpace(0, new int[]{}, 0, 4);
 
+        BoardSpace Pacific_Avenue = new BoardSpace(300, new int[]{26,130,390,900,1100,1275},
+                7,1);
+        BoardSpace North_Caroline_Avenue = new BoardSpace(300, new int[]{26,130,390,900,1100,1275},
+                7,1);
+        BoardSpace Community_Chest = new BoardSpace();
+
+        BoardSpace Pennsylvania_Avenue = new BoardSpace(320, new int[]{28, 150, 450, 1000, 1200, 1400},
+                7,1);
+        BoardSpace Short_Line = new BoardSpace(200, new int[]{25,50,100,200},
+                0,2);
+        BoardSpace Chance = new BoardSpace();
+
+        BoardSpace Park_Place = new BoardSpace(350, new int[]{35, 175, 500, 1100, 1300},
+                8,1);
+        BoardSpace Luxury_Tax = new BoardSpace();
+
+        BoardSpace Boardwalk = new BoardSpace(400, new int[]{50,200,600,1400,1700},8,1);
     }
 }
